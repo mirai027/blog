5ed8f2c58b0899b2d2880cf476879e87e78e6f24
mirai-white <1163302785@qq.com>
2020-09-01 20:32:15 +0800

    新增 gitalk评论功能

---
+++ docs/.vuepress/components/comment/comment.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="gitalk-container">
+    <div id="gitalk-container"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'comment',
+  data() {
+    return {}
+  },
+  mounted() {
+    let body = document.querySelector('.gitalk-container')
+    let script = document.createElement('script')
+    script.src = 'https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js'
+    body.appendChild(script)
+    script.onload = () => {
+      const commentConfig = {
+        clientID: '476c2c42f96e2582fde6',
+        clientSecret: '35a043dc2a0d561501ae0853fc75fab8e75bfc0f',
+        repo: 'mirai027.github.io',
+        owner: 'mirai027',
+        // 这里接受一个数组，可以添加多个管理员
+        admin: ['mirai027'],
+        // id 用于当前页面的唯一标识，一般来讲 pathname 足够了，
+
+        // 但是如果你的 pathname 超过 50 个字符，GitHub 将不会成功创建 issue，此情况可以考虑给每个页面生成 hash 值的方法.
+        id: location.pathname,
+        distractionFreeMode: false
+      }
+      const gitalk = new Gitalk(commentConfig)
+      gitalk.render('gitalk-container')
+    }
+  }
+}
+</script>
+
+<style>
+@import './gitalk.css';
+</style>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,15 +12,18 @@ module.exports = {
   ],
   description: '欲渡黄河冰塞川 将登太行雪满山',
   themeConfig: {
-    nav: [{
+    nav: [
+      {
         text: '顶级突破手',
         link: '/interview/'
       },
       {
         text: '枪械库',
-        items: [{
+        items: [
+          {
             text: '前端三剑客',
-            items: [{
+            items: [
+              {
                 text: 'HTML',
                 link: '/frontend/html/'
               },
@@ -36,7 +39,8 @@ module.exports = {
           },
           {
             text: '小程序',
-            items: [{
+            items: [
+              {
                 text: '微信小程序',
                 link: '/mini-program/wx/'
               },
@@ -48,16 +52,19 @@ module.exports = {
           },
           {
             text: '后端',
-            items: [{
-              text: 'Nodejs',
-              link: '/backend/nodejs/'
-            }]
+            items: [
+              {
+                text: 'Nodejs',
+                link: '/backend/nodejs/'
+              }
+            ]
           }
         ]
       },
       {
         text: 'Vue',
-        items: [{
+        items: [
+          {
             text: 'Vue 源码分析',
             link: '/vue/source-code/'
           },
@@ -69,7 +76,8 @@ module.exports = {
       },
       {
         text: '犯罪记录',
-        items: [{
+        items: [
+          {
             text: '掘金',
             link: 'https://juejin.im/user/4089838987133496/posts'
           },
@@ -81,49 +89,76 @@ module.exports = {
       }
     ],
     sidebar: {
-      '/interview/': [{
+      '/interview/': [
+        {
           title: 'Vue',
           collapsable: false,
           sidebarDepth: 2,
-          children: [
-            '/interview/vue/父子组件prop值的双向绑定'
-          ]
+          children: ['/interview/vue/父子组件prop值的双向绑定']
         },
         {
           title: 'nodejs',
           collapsable: false,
           sidebarDepth: 2,
+          children: ['/interview/nodejs/node包管理器']
+        }
+      ],
+      '/frontend/css/': [
+        {
+          title: 'CSS',
+          collapsable: false,
+          sidebarDepth: 1,
+          children: ['/frontend/css/adaptive']
+        }
+      ],
+      '/frontend/javascript/': [
+        {
+          title: 'Javascript',
+          collapsable: false,
+          sidebarDepth: 1,
           children: [
-            '/interview/nodejs/node包管理器'
+            '/frontend/javascript/Object.defineProperty',
+            '/frontend/javascript/prototype',
+            '/frontend/javascript/closure',
+            '/frontend/javascript/class'
           ]
         }
       ],
-      '/frontend/css/': [{
-        title: 'CSS',
-        collapsable: false,
-        sidebarDepth: 1,
-        children: ['/frontend/css/adaptive']
-      }],
-      '/frontend/javascript/': [{
-        title: 'Javascript',
-        collapsable: false,
-        sidebarDepth: 1,
-        children: [
-          '/frontend/javascript/Object.defineProperty',
-          '/frontend/javascript/prototype',
-          '/frontend/javascript/closure',
-          '/frontend/javascript/class'
-        ]
-      }],
-      '/vue/mini-vue/': [{
-        title: '迷你 Vue',
-        collapsable: false,
-        sidebarDepth: 2,
-        children: ['/vue/mini-vue/']
-      }]
+      '/vue/mini-vue/': [
+        {
+          title: '迷你 Vue',
+          collapsable: false,
+          sidebarDepth: 2,
+          children: ['/vue/mini-vue/']
+        }
+      ]
     },
     smoothScroll: true,
     lastUpdated: '最后更新时间',
-    logo: '/image/sidebar.png'
+    logo: '/image/sidebar.png',
+    plugins: [
+      [
+        'vuepress-plugin-mygitalk',
+        {
+          // 是否启用(关闭请设置为false)(default: true)
+          enable: true,
+          // 是否开启首页评论(default: true)
+          home: true,
+          // Gitalk配置
+          gitalk: {
+            // GitHub Application Client ID.
+            clientID: '476c2c42f96e2582fde6',
+            // GitHub Application Client Secret.
+            clientSecret: '35a043dc2a0d561501ae0853fc75fab8e75bfc0f',
+            // GitHub repository. 存储评论的 repo
+            repo: 'mirai027.github.io',
+            // GitHub repository 所有者，可以是个人或者组织。
+            owner: 'mirai027',
+            // 设置语言(default: zh-CN)
+            language: 'zh-CN'
+          }
+        }
+      ]
+    ]
   }
 }
