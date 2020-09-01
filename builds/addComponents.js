76b42c7d1ca51405e62020adf8bcc6c5734260cc
mirai-white <1163302785@qq.com>
2020-09-02 00:14:39 +0800

    删除 评论功能

--- builds/addComponents.js
+++ builds/addComponents.js
@@ -1,8 +1,16 @@
 const fs = require('fs')
 const findMarkdown = require('./findMarkdown')
-const rootDir = './docs'
+const rootDir = './docs/backend'
+const rootDir2 = './docs/frontend'
+const rootDir3 = './docs/interview'
+const rootDir4 = './docs/mini-program'
+const rootDir5 = './docs/vue'
 
 findMarkdown(rootDir, writeComponents)
+findMarkdown(rootDir2, writeComponents)
+findMarkdown(rootDir3, writeComponents)
+findMarkdown(rootDir4, writeComponents)
+findMarkdown(rootDir5, writeComponents)
 
 function writeComponents(dir) {
   fs.appendFile(dir, `\n \n <comment-comment/> \n `, (err) => {
